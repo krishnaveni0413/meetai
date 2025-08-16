5070014a70ca6f00112c7f2b38e236773800917a
krishnaveni0413 <krishnaveni.debnath04@gmail.com>
2025-08-16 19:04:55 +0530

    19:meetings filters

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -8,7 +8,15 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-const Page =  async () => {
+import { loadSearchParams } from "@/modules/meetings/params";
+import type { SearchParams } from "nuqs/server";
+
+interface Props{
+    searchParams: Promise<SearchParams>
+}
+
+const Page =  async ({ searchParams }:Props) => {
+    const filters = await loadSearchParams(searchParams)
      const session = await auth.api.getSession({
                 headers: await headers(),
             });
@@ -18,7 +26,9 @@ const Page =  async () => {
             }
     const queryClient = getQueryClient();
     void queryClient.prefetchQuery(
-        trpc.meetings.getMany.queryOptions({})
+        trpc.meetings.getMany.queryOptions({
+            ...filters,
+        })
     );
     return (
         <>
--- src/modules/meetings/server/ui/components/meetings-list-header.tsx
+++ src/modules/meetings/server/ui/components/meetings-list-header.tsx
@@ -4,13 +4,31 @@ import { Button } from "@/components/ui/button"
 import { PlusIcon, XCircleIcon } from "lucide-react"
 import { NewMeetingDialog } from "./new-meeting-dialog"
 import { useState } from "react"
+import { MeetingsSearchFilter } from "./meeting-search-filters"
+import { StatusFilter } from "./status-filter"
+import { AgentIdFilter } from "./agent-id-filter"
+import { useMeetingsFilters } from "@/modules/meetings/hooks/use-meetings-filters"
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+import { DEFAULT_PAGE } from "@/constants"
 
 export const MeetingListHeader = () => {
+    const [filters, setFilters] = useMeetingsFilters()
     const [isDialogOpen, setIsDialogOpen] = useState(false)
-    
+
+    const isAnyFilterModifed = !!filters.status || !!filters.search || !!filters.agentId;
+
+    const onClearFilers = () => {
+        setFilters({
+            status: null,
+            agentId: "",
+            search: "",
+            page: DEFAULT_PAGE,
+        })
+    }
+
     return (
         <>
-           <NewMeetingDialog open= {isDialogOpen} onOpenChange={setIsDialogOpen}/>
+            <NewMeetingDialog open={isDialogOpen} onOpenChange={setIsDialogOpen} />
             <div className="py-4 px-4 md:px-8 flex flex-col gap-y-4">
                 <div className="flex items-center justify-between">
                     <h5 className="font-medium text-xl">My Mettings</h5>
@@ -19,9 +37,20 @@ export const MeetingListHeader = () => {
                         New Mettings
                     </Button>
                 </div>
-                <div className="flex items-centergap-x-2 p-1">
-                    ToDo: Filters
-                </div>
+                <ScrollArea>
+                    <div className="flex items-center gap-x-2 p-1">
+                        <MeetingsSearchFilter />
+                        <StatusFilter />
+                        <AgentIdFilter />
+                        {isAnyFilterModifed && (
+                            <Button variant="outline" onClick={onClearFilers}>
+                                <XCircleIcon className="size-4" />
+                                Clear
+                            </Button>
+                        )}
+                    </div>
+                    <ScrollBar orientation="horizontal"/>
+                </ScrollArea>
             </div>
         </>
     )
